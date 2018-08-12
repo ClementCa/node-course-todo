76d3ae7ab9e567304c303a7262a3073a6a1759e8
Clement <clement.carretier@gmail.com>
2018-08-12 16:18:51 +0200

    get specific todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mogoose');
 var {Todo} = require("./models/todo");
@@ -31,6 +33,21 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e)
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    Todo.findById(req.params.id).then((todo) => {
+        if(!todo){
+            res.status(405).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     })
 });
 
