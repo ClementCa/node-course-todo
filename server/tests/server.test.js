096f00fb80e07e83cd47a87a85c16e680d8485c1
Clement <clement.carretier@gmail.com>
2018-08-12 15:50:02 +0200

    Add test get todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+},  {
+    text : 'Second test todo'
+}];
 
 beforeEach((done) => {
-   Todo.remove({}).then(() => done())
+   Todo.remove({}).then(() =>{
+       Todo.insertMany(todos);
+   }).then(() => done())
 });
 
 describe('Post /todos', () => {
@@ -26,7 +33,7 @@ describe('Post /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -45,9 +52,28 @@ describe('Post /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e))
             })
     })
+});
+
+
+
+describe('Get /todos', () => {
+    it('should get all todos', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .get('/todos')
+            .send()
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+                // expect(res.body.todos).toBe(todos);
+            })
+            .end(done);
+    });
+
 });
